6bf11540dcc73f98e76e5331a2d61aee147a108d
coinop-logan <coinop.logan@gmail.com>
2022-12-26 12:15:39 -0900

    Test out collision
    Co-authored-by: Rivera-Josh <Rivera-Josh@users.noreply.github.com>

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 static dWorldID world;
 static dSpaceID space;
 static dJointGroupID contactgroup;
-static dBodyID body1;
-static dGeomID geom1;
+static dBodyID body1, body2;
+static dGeomID geom1, geom2;
 
 void startPhysics()
 {
@@ -16,6 +16,8 @@ void startPhysics()
     world = dWorldCreate();
     space = dSimpleSpaceCreate(0);
 
+    // dWorldSetGravity(world, 0, 0, -9.8);
+
     dMass m;
     dMassSetBox (&m, 1, 0.5f ,0.5f, 0.5f);
     dMassAdjust (&m, 1);
@@ -23,10 +25,17 @@ void startPhysics()
     body1 = dBodyCreate(world);
     dBodySetMass(body1,&m);
     dBodySetPosition (body1, 0, 0, 1);
+    body2 = dBodyCreate(world);
+    dBodySetMass(body2,&m);
+    dBodySetPosition (body2, 1, 0, 1);
 
     geom1 = dCreateBox(space, 0.5, 0.5, 0.5);
+    geom2 = dCreateBox(space, 0.5, 0.5, 0.5);
 
     dGeomSetBody(geom1, body1);
+    dGeomSetBody(geom2, body2);
+
+    dBodyAddForce(body2, -10, 0, 0);
 }
 
 void cleanupPhysics()
@@ -43,8 +52,9 @@ static void nearCallback(void *, dGeomID o1, dGeomID o2)
     dBodyID b2 = dGeomGetBody(o2);
 
     dContact contact;
-    contact.surface.mode = 0;
+    contact.surface.mode = dContactBounce;
     contact.surface.mu = dInfinity;
+    contact.surface.bounce = 1;
     if (dCollide (o1,o2,1,&contact.geom,sizeof(dContactGeom))) {
         dJointID c = dJointCreateContact (world,contactgroup,&contact);
         dJointAttach (c,b1,b2);
@@ -66,7 +76,10 @@ int main()
     {
         stepPhysics();
         const dReal* pos = dBodyGetPosition(body1);
-        cout << pos[0] << ", " << pos[1] << ", " << pos[2] << endl;
+        cout << "1: " << pos[0] << ", " << pos[1] << ", " << pos[2] << endl;
+        pos = dBodyGetPosition(body2);
+        cout << "2: " << pos[0] << ", " << pos[1] << ", " << pos[2] << endl;
+        cin.get();
     }
 
     cleanupPhysics();
