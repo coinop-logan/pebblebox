66b75ac37f5ec0d73685b178ff6ac6b88cee0b7b
Josh R <joshrivera2203@gmail.com>
2022-12-26 12:42:50 -0900

    Added comments
    Co-authored-by: Logan <coinop.logan@gmail.com>

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,30 +3,42 @@
 
 using namespace std;
 
+
 static dWorldID world;
+
+//defines one group of objects that may interact
 static dSpaceID space;
-static dJointGroupID contactgroup;
+
+//Creates a group that will be filled with contact joints for all collisions on a given frame.
+//every frame, the group is emptied after simulation
+static dJointGroupID contactGroup;
+
+
 static dBodyID body1, body2;
 static dGeomID geom1, geom2;
 
 void startPhysics()
 {
+
     dInitODE();
-    contactgroup = dJointGroupCreate(0);
+
+    //init "contact" joint group
+    contactGroup = dJointGroupCreate(0);
+    
     world = dWorldCreate();
     space = dSimpleSpaceCreate(0);
 
     // dWorldSetGravity(world, 0, 0, -9.8);
 
-    dMass m;
-    dMassSetBox (&m, 1, 0.5f ,0.5f, 0.5f);
-    dMassAdjust (&m, 1);
+    dMass mass;
+    dMassSetBox (&mass, 1, 0.5f ,0.5f, 0.5f);
+    dMassAdjust (&mass, 1);
     
     body1 = dBodyCreate(world);
-    dBodySetMass(body1,&m);
+    dBodySetMass(body1,&mass);
     dBodySetPosition (body1, 0, 0, 1);
     body2 = dBodyCreate(world);
-    dBodySetMass(body2,&m);
+    dBodySetMass(body2,&mass);
     dBodySetPosition (body2, 1, 0, 1);
 
     geom1 = dCreateBox(space, 0.5, 0.5, 0.5);
@@ -40,32 +52,55 @@ void startPhysics()
 
 void cleanupPhysics()
 {
-    dJointGroupDestroy(contactgroup);
+    dJointGroupDestroy(contactGroup);
     dSpaceDestroy (space);
     dWorldDestroy (world);
     dCloseODE();
 }
 
+//called when two object are near each other
 static void nearCallback(void *, dGeomID o1, dGeomID o2)
 {
+    //two objects that might be colliding
     dBodyID b1 = dGeomGetBody(o1);
     dBodyID b2 = dGeomGetBody(o2);
 
+    //where we define the behavior of this collision using a structure `contact`
     dContact contact;
     contact.surface.mode = dContactBounce;
-    contact.surface.mu = dInfinity;
-    contact.surface.bounce = 1;
+
+    // "mu" is a physics term for friction
+    contact.surface.mu = dInfinity; //Full friction, no slippage
+
+    contact.surface.bounce = 1; // 1 = "full bounce"
+
+    //if there is a contact: dCollide returns the number of contacts (i.e. collisions) we need to handle.
+    //Here, the third argument "1" indicates we will only ever deal with 1 collision (otherwise we'd need a for loop and an array)
     if (dCollide (o1,o2,1,&contact.geom,sizeof(dContactGeom))) {
-        dJointID c = dJointCreateContact (world,contactgroup,&contact);
+
+        //Creates a "contact" type joint. Passes in world, joint contact group for collision, and a reference to the behavior of the contact.
+        dJointID c = dJointCreateContact (world,contactGroup,&contact);
+
+        //connects the bodies by the newly defined joint
         dJointAttach (c,b1,b2);
     }
 }
 
 void stepPhysics()
 {
+    // Below, note that the `contactGroup` is used as a sort of temporary container for all of the
+    // "contact" joints, which were created in nearCallback for every collision
+    // then these are cleared.
+    // The result is that when `dWorldQuickStep` is called, there are joints representing the constraints of current collisions
+
+    // This calls our nearCallback for every possible collision - which adds "contact" joints to contactGroup
     dSpaceCollide(space,0,&nearCallback);
+
+    // simulate the world for 0.05 seconds
     dWorldQuickStep (world,0.05);
-	dJointGroupEmpty(contactgroup);
+
+    // This clears all of the "contact" joints
+	dJointGroupEmpty(contactGroup);
 }
 
 int main()
